25fb6c84b4bd349412cb22d18ef78b27780d428c
ajeet <ajeetkumar23898@gmail.com>
2022-05-02 17:16:38 +0530

    ok

---
+++ lab3DsaSolution/src/balancingBracket/BalancingBracket.java
@@ -0,0 +1,48 @@
+package balancingBracket;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class BalancingBracket {
+
+	public static void balanceBracket(String str) {
+		Stack<Character> st = new Stack<>();
+		int size = str.length();
+		int check = 0;
+
+		for (int i = 0; i < size; i++) {
+			char temp = str.charAt(i);
+			System.out.println(temp + "temp");
+			if (temp == '(' || temp == '{' || temp == '[') {
+				st.push(temp);
+				check = 1;
+			} else {
+				char temp2 = st.peek();
+				int comp = Character.compare(temp, temp2);
+				
+// comp will return ASCII code difference 
+				if (comp == 2 || comp == 1) {
+					st.pop();
+					check = 0;
+				} else {
+					check = 1;
+				}
+			}
+		}
+
+		if (check == 0) {
+			System.out.println("The entered String has Balanced Brackets" + "");
+		} else {
+			System.out.println("The entered String do not contain Balanced Brackets" + "");
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		str = str.replaceAll("\\s", "");
+		balanceBracket(str);
+
+	}
+
+}
---
+++ lab3DsaSolution/src/pairWithGivenSum/PairWithGivenSum.java
@@ -0,0 +1,83 @@
+package pairWithGivenSum;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class PairWithGivenSum {
+	 static class Node {
+	        int data;
+	        Node left, right;
+	    };
+	 
+	    static Node NewNode(int data)
+	    {
+	        Node temp = new Node();
+	        temp.data = data;
+	        temp.left = null;
+	        temp.right = null;
+	        return temp;
+	    }
+	 
+	    static Node insert(Node root, int key)
+	    {
+	        if (root == null)
+	            return NewNode(key);
+	        if (key < root.data)
+	            root.left = insert(root.left, key);
+	        else
+	            root.right = insert(root.right, key);
+	        return root;
+	    }
+	 
+	    static boolean findpair(Node root, int sum,
+	                                HashSet<Integer> set)
+	    {
+	        if (root == null) {
+	            return false;
+	            }
+	 
+	        if (findpair(root.left, sum, set)) {
+	            return true;
+	            }
+	 
+	        if (set.contains(sum - root.data)) {
+	            System.out.println("Pair is ("
+	                               + (sum - root.data) + ", "
+	                               + root.data + ")");
+	            return true;
+	        }
+	        else {
+	            set.add(root.data);
+	            }
+	 
+	        return findpair(root.right, sum, set);
+	    }
+	 
+	    static void findPair(Node root, int sum)
+	    {
+	        HashSet<Integer> set = new HashSet<Integer>();
+	        if (!findpair(root, sum, set))
+	            System.out.print("Pairs do not exit"
+	                             + "\n");
+	    }
+	 
+	    // Driver code
+	    public static void main(String[] args)
+	    {
+	        Node root = null;
+	        root = insert(root, 25);
+	        root = insert(root, 50);
+	        root = insert(root, 28);
+	        root = insert(root, 18);
+	        root = insert(root, 12);
+	        root = insert(root, 26);
+	        root = insert(root, 25);
+	        root = insert(root, 15);
+	 
+	        int sum = 40;
+	        findPair(root, sum);
+	    }
+
+}
